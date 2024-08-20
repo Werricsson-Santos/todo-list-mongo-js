377ce52121b46cd4df2d4fe5d74fae29fca64035
Werricsson <werricsson.santos@hotmail.com>
2024-08-19 22:28:02 -0300

    :truck: Creating routes

---
+++ src/routes/checklist.js
@@ -0,0 +1,59 @@
+const express = require('express');
+
+const router = express.Router();
+
+const Checklist = require('../models/checklist');
+
+router.get('/', async (req, res) => {
+    try {
+        let checklists = await Checklist.find({});
+        res.status(200).json(checklists);
+        res.status(200).render('checklists/index', { checklists: checklists })
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+router.post('/', async (req, res) => {
+    let { name } = req.body;
+
+    try {
+        let checklist = await Checklist.create({ name })
+        res.status(200).json(checklist);
+    } catch (error) {
+        res.status(422).json(error);
+    }
+})
+
+router.get('/:id', async (req, res) => {
+    try {
+        let checklist = Checklist.findById(req.params.id);
+        res.status(200).json(checklist);
+        res.status(200).render('checklists/show', { checklist: checklist })
+
+    } catch (error) {
+        res.status(422).render('pages/error', { error: 'Erro ao exibir as listas de tarefas' });
+    }
+})
+
+router.put('/:id', async (req, res) => {
+    let { name } = req.body
+
+    try {
+        let checklist = Checklist.findByIdAndUpdate(req.params.id, { name }, { new: true });
+        res.status(200).json(checklist);
+    } catch (error) {
+        res.status(422).json(error);
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    try {
+        let checklist = Checklist.findByIdAndRemove(req.params.id);
+        res.status(200).json(checklist);
+    } catch (error) {
+        res.status(422).json(error);
+    }
+})
+
+module.exports = router;
