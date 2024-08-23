7894fc109fb16a37d39f947333032670fc831642
Werricsson <werricsson.santos@hotmail.com>
2024-08-22 22:00:00 -0300

    :bug: Fixing the query statement to await the response

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -7,8 +7,7 @@ const Checklist = require('../models/checklist');
 router.get('/', async (req, res) => {
     try {
         let checklists = await Checklist.find({});
-        res.status(200).json(checklists);
-        res.status(200).render('checklists/index', { checklists: checklists })
+        res.status(200).render('checklists/index', { checklists: checklists });
     } catch (error) {
         res.status(500).json(error);
     }
@@ -27,10 +26,8 @@ router.post('/', async (req, res) => {
 
 router.get('/:id', async (req, res) => {
     try {
-        let checklist = Checklist.findById(req.params.id);
-        res.status(200).json(checklist);
-        res.status(200).render('checklists/show', { checklist: checklist })
-
+        let checklist = await Checklist.findById(req.params.id);
+        res.status(200).render('checklists/show', { checklist: checklist });
     } catch (error) {
         res.status(422).render('pages/error', { error: 'Erro ao exibir as listas de tarefas' });
     }
