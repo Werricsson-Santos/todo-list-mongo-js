6f72a08fc1ea2b98f6c8dc3a6a43b9e65bdfbac5
Werricsson <werricsson.santos@hotmail.com>
2024-08-25 07:55:56 -0300

    :recycle: Create and use task router

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 
 const checklistRouter = require('./src/routes/checklist');
+const taskRouter = require('./src/routes/task');
+
 const rootRouter = require('./src/routes/index');
 const methodOverride = require('method-override');
 
@@ -19,6 +21,9 @@ app.set('view engine', 'ejs');
 
 app.use('/', rootRouter);
 app.use('/checklists', checklistRouter);
+app.use('/checklists', taskRouter.checklistDependent);
+app.use('/tasks', taskRouter.simple);
+
 
 app.listen(3000, () => {
     console.log('Servidor iniciado');
